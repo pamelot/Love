dca1ad28e3afc169e8e6fd0326a6079bd261873e
Pam M <iPam@PamMac.local>
2017-07-26 20:36:07 -0700

    added bcrypt import

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for, request, session)
-import bcrpyt
-from flask_bcrypt import check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 import forms
