fccbcab1ba43539bb495b2e6bbe4e582bc849595
Pam M <iPam@PamMac.local>
2017-07-26 20:58:10 -0700

    adding more requirements

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import (Flask, g, render_template, flash, redirect, url_for, request, session)
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
